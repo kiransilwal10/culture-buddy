3bc6ccaba973aa459b36d6ff3b879be0b2a18d46
kiransilwal10 <kiransilwal2002@gmail.com>
2024-11-07 23:32:42 -0600

    Login and signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { About } from "./components/About";
 import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
 import { Navbar } from "./components/Navbar";
 import { ScrollToTop } from "./components/ScrollToTop";
+import Signup from "./components/pages/Signup";
+import Login from "./components/pages/Login";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Hero />
-      <About />
-      <Footer />
+      <Routes>
+        {/* Main Page */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <About />
+              <Footer />
+            </>
+          }
+        />
+
+        {/* Signup and Login Pages */}
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <ScrollToTop />
-    </>
+    </Router>
   );
 }
 
